d4c09e0263b41b208f2347aff9d0f4e6752c21f0
Leandro Acquati <leandroacquati@gmail.com>
2018-02-18 01:57:42 -0300

    teste porta

--- data/actions/scripts/doors/level_12.lua
+++
@@ -1,5 +0,0 @@
-local level = 12
-
-function onUse(cid, fromPos, toPos)
-	return getPlayerLevel(cid) >= level and doTeleportThing(cid, toPos) or doPlayerSendTextMessage(cid, MESSAGE_INFO_DESCR, 'Only players level '..level..' or higher can open this door.') and false
-end
