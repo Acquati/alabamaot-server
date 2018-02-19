2264886415545a65b0b348157fa4ad37c383e5f8
Leandro Acquati <leandroacquati@gmail.com>
2018-02-19 05:45:35 -0300

    teste tpfalante

--- data/globalevents/scripts/tpfalante.lua
+++ data/globalevents/scripts/tpfalante.lua
@@ -1,4 +1,5 @@
-function onThink(interval, lastExecution)
-	pos = {x = 1024, y = 1028, z = 7} -- Coordenadas do teleport
-	doSendAnimatedText(pos, 'Hunts', 140) -- Ira mandar Texto animado na pos
+function onThink(cid, interval, lastExecution)
+	local pos = {x = 1024, y = 1028, z = 7} -- Coordenadas do teleport
+	pos:sendTextMessage('Hunts') -- Ira mandar Texto animado na pos
+	return true
 end
