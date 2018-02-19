db81f5511c7a0c3cc7eafed35629109e25a91a15
Leandro Acquati <leandroacquati@gmail.com>
2018-02-19 05:23:50 -0300

    teste tpfalante

--- data/globalevents/scripts/tpfalante.lua
+++ data/globalevents/scripts/tpfalante.lua
@@ -1,17 +1,5 @@
-function onThink(cid, interval, lastExecution)
-
-local config = {
-  --[cor] = {pos},
-    [140] = {x = 1024, y = 1028, z = 7},
-    [123] = {x = 1024, y = 1028, z = 7},
-    }  
-    
-    for cor, pos in pairs(config) do              
-        doSendAnimatedText(pos, "Batata", cor) -- Configurar texto aqui
-          doSendMagicEffect(pos, 11) 
-
-            doSendAnimatedText(pos, "Cenoura", cor)
-            doSendMagicEffect(pos, 13)
-       end
-return true
+function onThink(interval, lastExecution)
+	pos = {x = 1024, y = 1028, z = 7} -- Coordenadas do teleport
+	doSendAnimatedText(pos, 'Hunts', 140) -- Ira mandar Texto animado na pos
+	return true
 end
