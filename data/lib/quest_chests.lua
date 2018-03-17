aec43c86f16b74078e67a7815b45a473add2386d
Leandro Acquati <leandroacquati@gmail.com>
2018-03-17 12:59:43 -0300

    test chest

--- data/lib/quest_chests.lua
+++ data/lib/quest_chests.lua
@@ -1,32 +1,293 @@
 questChests = {
-    ---------------------------------------------------------
-   -- Example Quest Box 1
-   ---------------------------------------------------------
-
-   --[[
-   [35700] = {
-       minLevel = 8,
-       items = {
-           [1] = {type = "item", item = 7425, count = 1},
-           [2] = {type = "experience", amount = 2000},
-           [3] = {type = "outfit", name = "assassin", femaleId = 156, maleId = 152},
-           [4] = {type = "addon", outfit = "nobleman", addonNumber = 1, femaleId = 140, maleId = 132},
-           [5] = {type = "mount", mountName = "Orc", mountId = 20},
-       },
-   },
-   --]]
-   [35700] = {
-       minLevel = 8,
-       items = {
-           [1] = {type = "item", item = 7425, count = 1},
-           [2] = {type = "experience", amount = 2000},
-       },
-   },
-   [35701] = {
-       minLevel = 8,
-       items = {
-           [1] = {type = "item", item = 7430, count = 1},
-           [2] = {type = "experience", amount = 2000},
-       },
-   },
-}
+---------------------------------------------------------
+-- Example Quest Box 1
+---------------------------------------------------------
+--[[
+[35700] = {
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 7425, count = 1},
+    [2] = {type = "experience", amount = 2000},
+    [3] = {type = "outfit", name = "assassin", femaleId = 156, maleId = 152},
+    [4] = {type = "addon", outfit = "nobleman", addonNumber = 1, femaleId = 140, maleId = 132},
+    [5] = {type = "mount", mountName = "Orc", mountId = 20},
+  },
+},
+--]]
+[35700] = { -- Cave Rat
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 3}, -- platinum coin
+    [2] = {type = "item", item = 2696, count = 5},
+    [3] = {type = "experience", amount = 1000},
+  },
+},
+[35701] = { -- Killer Rabbit
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 18}, -- platinum coin
+    [2] = {type = "experience", amount = 8000},
+  },
+},
+[35702] = { -- Mutated Rat
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 26}, -- platinum coin
+    [2] = {type = "item", item = 8900, count = 1},
+    [3] = {type = "experience", amount = 22500},
+  },
+},
+[35703] = { -- Rotworm
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2148, count = 80}, -- gold coin
+    [2] = {type = "item", item = 2152, count = 6}, -- platinum coin
+    [3] = {type = "experience", amount = 4000},
+  },
+},
+[35704] = { -- Carrion Worm
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2148, count = 80}, -- gold coin
+    [2] = {type = "item", item = 2152, count = 9}, -- platinum coin
+    [3] = {type = "experience", amount = 3500},
+  },
+},
+[35705] = { -- Drillworm
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 30}, -- platinum coin
+    [2] = {type = "item", item = 7887, count = 1},
+    [3] = {type = "item", item = 7452, count = 1},
+    [4] = {type = "experience", amount = 55000},
+  },
+},
+[35706] = { -- Frost Troll
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2148, count = 80}, -- gold coin
+    [2] = {type = "item", item = 2152, count = 4}, -- platinum coin
+    [3] = {type = "experience", amount = 2300},
+  },
+},
+[35707] = { -- Troll Champion
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2148, count = 80}, -- gold coin
+    [2] = {type = "item", item = 2152, count = 4}, -- platinum coin
+    [3] = {type = "item", item = 2170, count = 1},
+    [4] = {type = "experience", amount = 4000},
+  },
+},
+[35708] = { -- Troll Legionnaire
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 31}, -- platinum coin
+    [2] = {type = "item", item = 2165, count = 1},
+    [3] = {type = "experience", amount = 7000},
+  },
+},
+[35709] = { -- Furious Troll
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2148, count = 80}, -- gold coin
+    [2] = {type = "item", item = 2152, count = 55}, -- platinum coin
+    [3] = {type = "item", item = 2391, count = 1},
+    [4] = {type = "experience", amount = 9250},
+  },
+},
+[35710] = { -- Insectoid Scout
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 16}, -- platinum coin
+    [2] = {type = "item", item = 2159, count = 1},
+    [3] = {type = "item", item = 2964, count = 1},
+    [4] = {type = "item", item = 2439, count = 1},
+    [5] = {type = "experience", amount = 7500},
+  },
+},
+[35711] = { -- Ancient Scarab
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 38}, -- platinum coin
+    [2] = {type = "item", item = 7903, count = 1},
+    [3] = {type = "item", item = 2440, count = 1},
+    [4] = {type = "item", item = 2159, count = 15},
+    [5] = {type = "experience", amount = 36000},
+  },
+},
+[35712] = { -- Spitter
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 58}, -- platinum coin
+    [2] = {type = "item", item = 2497, count = 1},
+    [3] = {type = "item", item = 2171, count = 1},
+    [4] = {type = "experience", amount = 55000},                                                        
+  },                      
+},
+[35713] = { -- Orc Warrior
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 6}, -- platinum coin
+    [2] = {type = "item", item = 2530, count = 1},
+    [3] = {type = "item", item = 2411, count = 1},
+    [4] = {type = "experience", amount = 5000},
+  },
+},
+[35714] = { -- Orc Rider
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2148, count = 20}, -- gold coin
+    [2] = {type = "item", item = 2152, count = 16}, -- platinum coin
+    [3] = {type = "item", item = 2425, count = 1},
+    [4] = {type = "item", item = 2188, count = 1},
+    [5] = {type = "experience", amount = 5500},
+  },
+},
+[35715] = { -- Orc Marauder
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 18}, -- platinum coin
+    [2] = {type = "item", item = 8857, count = 1},
+    [3] = {type = "item", item = 7620, count = 20},
+    [4] = {type = "experience", amount = 10250},
+  },
+},
+[35716] = { -- Orc Leader
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 7}, -- platinum coin
+    [2] = {type = "item", item = 2475, count = 1},
+    [3] = {type = "item", item = 2413, count = 1},
+    [4] = {type = "experience", amount = 13500},
+  },
+},
+[35717] = { -- Orc Warlord
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 9}, -- platinum coin
+    [2] = {type = "item", item = 7891, count = 1},
+    [3] = {type = "item", item = 2497, count = 1},
+    [4] = {type = "item", item = 7395, count = 1},
+    [5] = {type = "experience", amount = 33500},
+  },
+},
+[35718] = { -- Minotaur
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2148, count = 50}, -- gold coin
+    [2] = {type = "item", item = 2152, count = 2}, -- platinum coin
+    [3] = {type = "item", item = 2172, count = 1},
+    [4] = {type = "item", item = 12428, count = 5},
+    [5] = {type = "experience", amount = 2500},
+  },
+},
+[35719] = { -- Minotaur Archer
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 6}, -- platinum coin
+    [2] = {type = "item", item = 2483, count = 1},
+    [3] = {type = "item", item = 2455, count = 1},
+    [4] = {type = "experience", amount = 3250},
+  },
+},
+[35720] = { -- Minotaur Guard
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 4}, -- platinum coin
+    [2] = {type = "item", item = 2387, count = 1},
+    [3] = {type = "item", item = 7618, count = 10},
+    [4] = {type = "experience", amount = 8000},
+  },
+},
+[35721] = { -- Minotaur Mage
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 7}, -- platinum coin
+    [2] = {type = "item", item = 2189, count = 1},
+    [3] = {type = "item", item = 7425, count = 1},
+    [4] = {type = "experience", amount = 7500},
+  },
+},
+[35722] = { -- Cyclops Drone
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 5}, -- platinum coin
+    [2] = {type = "item", item = 2490, count = 1},
+    [3] = {type = "item", item = 2381, count = 1},
+    [4] = {type = "experience", amount = 10000},
+  },
+},
+[35723] = { -- Cyclops Smith
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 14}, -- platinum coin
+    [2] = {type = "item", item = 7452, count = 1},
+    [3] = {type = "item", item = 7398, count = 1},
+    [4] = {type = "experience", amount = 12750},
+  },
+},
+[35724] = { -- Dragon 1
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 11}, -- platinum coin
+    [2] = {type = "item", item = 7409, count = 1},
+    [3] = {type = "item", item = 2187, count = 1},
+    [4] = {type = "experience", amount = 17500},
+  },
+},
+[35725] = { -- Dragon 2
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 11}, -- platinum coin
+    [2] = {type = "item", item = 2516, count = 1},
+    [3] = {type = "item", item = 7430, count = 1},
+    [4] = {type = "experience", amount = 17500},
+  },
+},
+[35726] = { -- Dragon Lord 1
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 25}, -- platinum coin
+    [2] = {type = "item", item = 2528, count = 1},
+    [3] = {type = "item", item = 2479, count = 1},
+    [4] = {type = "experience", amount = 52500},
+  },
+},
+[35727] = { -- Dragon Lord 2
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 25}, -- platinum coin
+    [2] = {type = "item", item = 2392, count = 1},
+    [3] = {type = "item", item = 2498, count = 1},
+    [4] = {type = "experience", amount = 52500},
+  },
+},
+[35728] = { -- Tarantula
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 8}, -- platinum coin
+    [2] = {type = "item", item = 2169, count = 1},
+    [3] = {type = "item", item = 2457, count = 1},
+    [4] = {type = "experience", amount = 6000},
+  },
+},
+[35729] = { -- Giant Spider
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 38}, -- platinum coin
+    [2] = {type = "item", item = 7901, count = 1},
+    [3] = {type = "item", item = 2476, count = 1},
+    [4] = {type = "experience", amount = 45000},
+  },
+},
+[35730] = { -- Pirate Corsair
+  minLevel = 8,
+  items = {
+    [1] = {type = "item", item = 2152, count = 17}, -- platinum coin
+    [2] = {type = "item", item = 5462, count = 1},
+    [3] = {type = "item", item = 5926, count = 1},
+    [4] = {type = "experience", amount = 17500},
+  },
+},
+} -- THE END
